c9ea64d3e08e606cdabaa20b3080cacb21d1b01d
Bartosz Leśniewski <blesniewski@outlook.com>
2022-06-11 01:40:40 +0200

    Add new heuristic function and clean heuristics code

--- clashes/human_vs_minimax.py
+++ clashes/human_vs_minimax.py
@@ -7,7 +7,7 @@ from player import Player
 
 board = Board(3)
 human_player = Player(board, "x")
-ai_player = AIPlayer(board, "o", AI_type.MINIMAX, 2, heuristics.heuristic_function)
+ai_player = AIPlayer(board, "o", AI_type.MINIMAX, 3, heuristics.extended_heuristic)
 
 game = Game(board, human_player, ai_player)
 game.play()
--- heuristics.py
+++ heuristics.py
@@ -17,7 +17,7 @@ def calculate_score(row, score, token: str, opponent_token: str):
     return score
 
 
-def heuristic_function(board, token):
+def advanced_heuristic(board, token):
     score = 0
     if token == "x":
         opponent_token = "o"
@@ -60,7 +60,9 @@ def heuristic_function(board, token):
     return score
 
 
-def update_wins_counter(str_row, x_wins, o_wins):
+def update_wins_counter(row, x_wins, o_wins):
+    str_row = ''.join(state for state in row)
+
     if 'xx_' in str_row or '_xx' in str_row or 'x_x' in str_row:
         x_wins += 1
     elif 'yy_' in str_row or '_yy' in str_row or 'y_y' in str_row:
@@ -76,8 +78,7 @@ def simple_heuristic(board, token):
 
     # check rows
     for row in board.states:
-        str_row = ''.join(state for state in row)
-        x_wins, o_wins = update_wins_counter(str_row, x_wins, o_wins)
+        x_wins, o_wins = update_wins_counter(row, x_wins, o_wins)
 
     # check columns
     for i in range(board.size):
@@ -85,39 +86,22 @@ def simple_heuristic(board, token):
         for j in range(board.size):
             column.append(board.states[j][i])
 
-        str_row = ''.join(state for state in column)
-        x_wins, o_wins = update_wins_counter(str_row, x_wins, o_wins)
+        x_wins, o_wins = update_wins_counter(column, x_wins, o_wins)
 
     # check main diagonals
     left_diagonal = []
     for j in range(board.size):
         left_diagonal.append(board.states[j][j])
 
-    str_row = ''.join(state for state in left_diagonal)
-    x_wins, o_wins = update_wins_counter(str_row, x_wins, o_wins)
+    x_wins, o_wins = update_wins_counter(left_diagonal, x_wins, o_wins)
 
     right_diagonal = []
     for j in range(board.size):
         right_diagonal.append(board.states[j][board.size - j - 1])
 
-    str_row = ''.join(state for state in right_diagonal)
-    x_wins, o_wins = update_wins_counter(str_row, x_wins, o_wins)
+    x_wins, o_wins = update_wins_counter(right_diagonal, x_wins, o_wins)
 
-    # check diagonals under the main diagonal (for larger sizes)
-    for i in range(1, board.size):
-        if board.size - i < 3:
-            break
-
-        col = 0
-        diagonal = []
-        for j in range(i, board.size):
-            diagonal.append(board.states[j][col])
-            col += 1
-
-        str_row = ''.join(state for state in diagonal)
-        x_wins, o_wins = update_wins_counter(str_row, x_wins, o_wins)
-
-    # check diagonals above the main diagonal (for larger sizes)
+    # check diagonals under and above the main diagonal (for larger sizes)
     for i in range(1, board.size):
         if board.size - i < 3:
             break
@@ -130,12 +114,88 @@ def simple_heuristic(board, token):
             diagonal_above.append(board.states[col][j])
             col += 1
 
-        str_row = ''.join(state for state in diagonal_under)
-        x_wins, o_wins = update_wins_counter(str_row, x_wins, o_wins)
-        str_row = ''.join(state for state in diagonal_above)
-        x_wins, o_wins = update_wins_counter(str_row, x_wins, o_wins)
+        x_wins, o_wins = update_wins_counter(diagonal_under, x_wins, o_wins)
+        x_wins, o_wins = update_wins_counter(diagonal_above, x_wins, o_wins)
 
     if token == "x":
         return x_wins - o_wins
     else:
         return o_wins - x_wins
+
+
+def update_tokens_with_blanks_counter(x1, x2, o1, o2, token: str, opponent_token: str, row):
+    str_row = ''.join(state for state in row)
+
+    if (token + "_" * 2 in str_row) or ("_" * 2 + token in str_row) or ("_" + token + "_" in str_row):
+        x1 += 1
+    elif (token * 2 + "_" in str_row) or ("_" + token * 2 in str_row) or (token + "_" + token in str_row):
+        x2 += 1
+    elif (opponent_token * 2 + "_" in str_row) or ("_" + opponent_token * 2 in str_row) or (opponent_token + "_" + opponent_token in str_row):
+        o2 += 1
+    elif (opponent_token + "_" * 2 in str_row) or ("_" * 2 + opponent_token in str_row) or ("_" + opponent_token + "_"):
+        o1 += 1
+
+    return x1, x2, o1, o2
+
+
+""" 3 * X2 + X1 - (3 * O2 + O1)
+Where:
+X1 is the number of lines with 1 X and 2 blanks
+X2 is the number of lines with 2 X's and a blank
+O1 is the number of lines with 1 O and 2 blanks
+O2 is the number of lines with 2 O's and a blank """
+
+
+def extended_heuristic(board, token):
+    if token == "x":
+        opponent_token = "o"
+    else:
+        opponent_token = "x"
+
+    x1 = x2 = o1 = o2 = 0
+
+    # check rows
+    for row in board.states:
+        x1, x2, o1, o2 = update_tokens_with_blanks_counter(x1, x2, o1, o2, token, opponent_token, row)
+
+    # check columns
+    for i in range(board.size):
+        column = []
+        for j in range(board.size):
+            column.append(board.states[j][i])
+
+        x1, x2, o1, o2 = update_tokens_with_blanks_counter(x1, x2, o1, o2, token, opponent_token, column)
+
+    # check main diagonals
+    left_diagonal = []
+    for j in range(board.size):
+        left_diagonal.append(board.states[j][j])
+
+    x1, x2, o1, o2 = update_tokens_with_blanks_counter(x1, x2, o1, o2, token, opponent_token, left_diagonal)
+
+    right_diagonal = []
+    for j in range(board.size):
+        right_diagonal.append(board.states[j][board.size - j - 1])
+
+    x1, x2, o1, o2 = update_tokens_with_blanks_counter(x1, x2, o1, o2, token, opponent_token, right_diagonal)
+
+    # check diagonals under and above the main diagonal (for larger sizes)
+    for i in range(1, board.size):
+        if board.size - i < 3:
+            break
+
+        col = 0
+        diagonal_under = []
+        diagonal_above = []
+        for j in range(i, board.size):
+            diagonal_under.append(board.states[j][col])
+            diagonal_above.append(board.states[col][j])
+            col += 1
+
+        x1, x2, o1, o2 = update_tokens_with_blanks_counter(x1, x2, o1, o2, token, opponent_token, diagonal_under)
+        x1, x2, o1, o2 = update_tokens_with_blanks_counter(x1, x2, o1, o2, token, opponent_token, diagonal_above)
+
+    if token == "x":
+        return 3 * x2 + x1 - (3 * o2 + o1)
+    else:
+        return 3 * o2 + o1 - (3 * x2 + x1)
